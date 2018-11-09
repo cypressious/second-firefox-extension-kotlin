42e49f7c412f2a6afd4f7f5c917d1b1a3a856315
Jean-Michel Fayard <jmfayard@gmail.com>
2018-11-09 10:00:31 +0100

    Upgrade Gradle build
    Upgrade to Gradle 4.10.2
    
    Move to the newer plugins syntax instead of buildscript { ... }
    
    Leverage my Gradle plugin for managing dependencies
    https://github.com/jmfayard/gradle-kotlin-dsl-libs
    
    Move build logic to the `buildSrc` as explained here
    https://docs.gradle.org/4.10/userguide/organizing_gradle_projects.html#sec:build_sources
    
    Apply the buildScan plugin, super handy for resolving problems.

---
+++ buildSrc/src/main/kotlin/Config.kt
@@ -0,0 +1,9 @@
+object Config {
+
+  val pluginsResolution : Map<String, String> = mapOf(
+    "kotlin-android" to Libs.kotlin_gradle_plugin,
+    "kotlin-dce-js" to Libs.kotlin_gradle_plugin,
+    "kotlin2js" to Libs.kotlin_gradle_plugin)
+
+
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,25 @@
+/**
+ * Find which updates are available by running
+ *     `$ ./gradlew syncLibs`
+ * This will only update the comments.
+ *
+ * YOU are responsible for updating manually the dependency version. */
+object Versions {
+    const val webextensions_declarations: String = "v0.1" //available: "v0.3" 
+
+    const val build_scan_plugin: String = "1.16" //available: "2.0.2" 
+
+    const val gradle_kotlin_dsl_libs: String = "0.2.6" 
+
+    const val org_jetbrains_kotlin: String = "1.2.0" //available: "1.3.0" 
+
+
+    const val gradleRunningVersion: String = "4.3.1"
+
+    const val gradleCurrentVersion: String = "4.10.2"
+
+    const val gradleNightlyVersion: String = "5.1-20181109000105+0000"
+
+    const val gradleReleaseCandidate: String = "5.0-rc-1"
+
+}
